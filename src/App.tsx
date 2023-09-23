37be5f29bd93674e2e570aea06c0bc9cdf0bc04b
BenHunt17 <58478479+BenHunt17@users.noreply.github.com>
2023-09-23 20:26:33 +0100

    attempt to call backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const uri = process.env.REACT_APP_MAILLARD_API_BASE_URI;
+
 function App() {
-  return <p>test</p>;
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    fetch(`${uri}/`, { method: "get" }).then((response) =>
+      response.text().then((v) => setData(v))
+    );
+  }, []);
+
+  return <p>{data}</p>;
 }
 
 export default App;
