632b227fc04907687deafe019a88fcf38d201bab
BenHunt17 <58478479+BenHunt17@users.noreply.github.com>
2023-11-05 18:00:42 +0000

    Update RecipeCollectionView.tsx

--- src/recipes/collection/view/RecipeCollectionView.tsx
+++ src/recipes/collection/view/RecipeCollectionView.tsx
@@ -1,7 +1,3 @@
-import { useAuth } from "../../../core/authentication/AuthProvider";
-
 export default function RecipeCollectionView() {
-  const { adminIsLoggedIn } = useAuth();
-  
   return <p>Recipes</p>;
 }
